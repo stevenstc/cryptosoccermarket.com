78e38d7163d0391bb95d96335642bc5ec6252b8f
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-09-29 20:46:39 -0500

    old new

---
+++ precache-manifest.01eaadd0318526daaeb99f8314833150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2d6d05d023f4b5daaaa19d9d4c3c05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aedc4a65aa09100bc02f",
+    "url": "/static/js/2.e41db314.chunk.js"
+  },
+  {
+    "revision": "bd494ecbaddc1ef1961b05601330b12d",
+    "url": "/static/js/2.e41db314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039a2afdb14048ae1d94",
+    "url": "/static/js/main.315f6f9b.chunk.js"
+  },
+  {
+    "revision": "33b59ce200bddee79b2b",
+    "url": "/static/js/runtime-main.27da0dc2.js"
+  }
+]);
